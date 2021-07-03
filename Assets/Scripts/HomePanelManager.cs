ca8deb2a41a0dc08cf58c99ac90191f280411a5b
Gen-3 <be.pestalozzi@gmail.com>
2021-07-03 18:26:08 +0900

    adjustChangeEquipPanelAtHome(in progress)
    ヒエラルキー上の作業を終えたところ
    このあとスクリプトを直す

---
+++ Assets/Scripts/HomePanelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomePanelManager : MonoBehaviour
+{
+    [SerializeField]GameObject changeEquipPanelAtHome = default;
+
+    public void OpenPanel()
+    {
+        changeEquipPanelAtHome.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        changeEquipPanelAtHome.SetActive(false);
+    }
+
+
+}
